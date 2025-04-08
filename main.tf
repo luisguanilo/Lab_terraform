82d071ecb4a7892c408ea50cffd00358229b7d6f
luisguanilo <guanilo99@gmail.com>
2025-04-07 22:14:28 -0500

    Actividad semana 03 - terraform

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_container" "container_server_web" {
+  name  = "server_web"
+  image = "nginx:latest"
+  ports {
+    external = 4005
+    internal = 80
+  }
+}
