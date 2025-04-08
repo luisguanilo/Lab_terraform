e74765dc1b0d757c3a48712a1b07e49d20a0a25a
luisguanilo <guanilo99@gmail.com>
2025-04-07 22:17:37 -0500

    actualizacion del puerto 4000 al 6000

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "docker_container" "container_server_web" {
   name  = "server_web"
   image = "nginx:latest"
   ports {
-    external = 4005
+    external = 6000
     internal = 80
   }
 }
